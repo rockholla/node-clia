cd797a1329b3608debff8903b97af0e85388bdc3
Patrick Force <patrickforce@gmail.com>
2018-08-09 18:55:24 -0600

    rework requirements checking a bit for catching, write some updated tests

--- src/requirements/index.js
+++ src/requirements/index.js
@@ -1,5 +1,6 @@
 import shell from 'shelljs'
 import semver from 'semver'
+import commandExists from 'command-exists'
 
 class Requirements {
 
@@ -15,9 +16,10 @@ class Requirements {
     if (engines instanceof Object) {
       Object.keys(engines).forEach((engineName) => {
         try {
+          let executableVersion = this.getExecutableVersion(engineName)
           this.testSemver(
             engineName,
-            this.getExecutableVersion(engineName),
+            executableVersion,
             engines[engineName])
         } catch (error) {
           this.errors.push(error)
@@ -27,9 +29,10 @@ class Requirements {
     if (requirements.executables instanceof Array) {
       requirements.executables.forEach((requirement) => {
         try {
+          let executableVersion = this.getExecutableVersion(requirement.name, requirement.version.command, requirement.version.replace)
           this.testSemver(
             requirement.name,
-            this.getExecutableVersion(requirement.name, requirement.version.command, requirement.version.replace),
+            executableVersion,
             requirement.version.required,
             requirement.help
           )
@@ -40,16 +43,22 @@ class Requirements {
     }
     if (this.errors.length > 0) {
       throw this.errors.join('')
+    } else {
+      return true
     }
   }
 
   getExecutableVersion (name, command, replace) {
-    command   = (command && command != '') ? command : `${name} --version`
-    replace   = (replace && replace != '') ? replace : this.defaultReplace
-    try {
-      return shell.exec(command, { silent: true }).replace(new RegExp(replace, 'g'), '').trim()
-    } catch (error) {
-      return error
+    command     = (command && command != '') ? command : `${name} --version`
+    replace     = (replace && replace != '') ? replace : this.defaultReplace
+    if (!commandExists.sync(name)) {
+      throw `${name} does not exist`
+    }
+    let result  = shell.exec(command, { silent: true })
+    if (result.code !== 0) {
+      throw `${result.stderr}`
+    } else {
+      return result.stdout.replace(new RegExp(replace, 'g'), '').trim()
     }
   }
 
--- tests/requirements/index.test.js
+++ tests/requirements/index.test.js
@@ -1,8 +1,81 @@
+import { Requirements } from '../../lib/requirements'
+
 describe('requirements/index', () => {
 
-  // TODO: unit test the requirements class
-  it('placeholder', () => {
-    expect(true).toEqual(true)
+  it('single executable requirement check should succeed', () => {
+    const requirements = new Requirements()
+    expect(requirements.check({
+      enabled: true,
+      executables: [
+        {
+          name: 'node',
+          version: {
+            required: '>=8',
+            command: 'node --version',
+            replace: '[a-zA-Z\\s]+'
+          },
+          help: ''
+        }
+      ]
+    })).toEqual(true)
+  })
+
+  it('single executable requirement check should succeed without a semantic version requirement', () => {
+    const requirements = new Requirements()
+    expect(requirements.check({
+      enabled: true,
+      executables: [
+        {
+          name: 'node',
+          version: {
+            required: '',
+            command: 'node --version',
+            replace: '[a-zA-Z\\s]+'
+          },
+          help: ''
+        }
+      ]
+    })).toEqual(true)
+  })
+
+  it('single executable requirement check should throw an error for a binary that does not exist', () => {
+    const requirements = new Requirements()
+    expect(() => {
+      requirements.check({
+        enabled: true,
+        executables: [
+          {
+            name: 'doesntexist',
+            version: {
+              required: '>=1',
+              command: 'doesntexist --version',
+              replace: '[a-zA-Z\\s]+'
+            },
+            help: ''
+          }
+        ]
+      })
+    }).toThrow()
+  })
+
+  it('single executable requirement check should throw an error for a binary that does not exist and without a version requirement', () => {
+    const requirements = new Requirements()
+    expect(() => {
+      requirements.check({
+        enabled: true,
+        executables: [
+          {
+            name: 'doesntexist',
+            version: {
+              required: '>=1',
+              command: 'doesntexist --version',
+              replace: '[a-zA-Z\\s]+'
+            },
+            help: ''
+          }
+        ]
+      })
+    }).toThrow()
   })
 
 })
